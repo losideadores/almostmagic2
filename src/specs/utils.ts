cc7d2eda0bce2bfa1d995b4ba8d87cb5b28f1e1e
Vova Zakharov <vzakharov@gmail.com>
2023-10-09 12:02:06 +0300

    utils as functions

--- src/specs/utils.ts
+++ src/specs/utils.ts
@@ -4,16 +4,18 @@ const findKey = <O extends Record<string, any>>(obj: O, predicate: (value: any) 
 const endsWith = <S extends string>(str: string, suffix: S): str is `${string}${S}` => str.endsWith(suffix);
 const startsWith = <S extends string>(str: string, prefix: S): str is `${S}${string}` => str.startsWith(prefix);
 
-export const matchesTemplate = <T extends EPSTemplate>(
-  str: string, [exact, prefix, suffix]: T
-): str is MatchesTemplate<T> => 
-  str === exact || !!prefix && startsWith(str, prefix) || !!suffix && endsWith(str, suffix);
+export function matchesTemplate<T extends EPSTemplate>(str: string, [exact, prefix, suffix]: T): str is MatchesTemplate<T> {
+  return str === exact || !!prefix && startsWith(str, prefix) || !!suffix && endsWith(str, suffix);
+};
 
-export const typeBasedOnSpecValue = (specValue: string): SpecType | undefined => 
-  findKey(specValueTemplates, template => matchesTemplate(specValue, template));
+export function typeBasedOnSpecValue(specValue: string): SpecType | undefined {
+  return findKey(specValueTemplates, template => matchesTemplate(specValue, template));
+};
 
-export const typeBasedOnSpecKey = (specKey: string): SpecType | undefined =>
-  findKey(specKeyTemplates, template => matchesTemplate(specKey, template));
+export function typeBasedOnSpecKey(specKey: string): SpecType | undefined {
+  return findKey(specKeyTemplates, template => matchesTemplate(specKey, template));
+};
 
-export const typeBasedOnSpecEntry = <S extends Record<string, string>>(spec: S, key: keyof S): SpecType | undefined =>
-  typeBasedOnSpecKey(key as string) || typeBasedOnSpecValue(spec[key]);
+export function typeBasedOnSpecEntry<S extends Record<string, string>>(spec: S, key: keyof S): SpecType | undefined {
+  return typeBasedOnSpecKey(key as string) || typeBasedOnSpecValue(spec[key]);
+};
