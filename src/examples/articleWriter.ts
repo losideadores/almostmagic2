8dd40f6dec6ee19490d066198eccab5e5b0dd101
Vova Zakharov <vzakharov@gmail.com>
2023-10-09 20:30:38 +0300

    shorten

--- src/examples/articleWriter.ts
+++ src/examples/articleWriter.ts
@@ -1,16 +1,14 @@
 import { MatchingOutput, Specs, generate } from "..";
 import { JsonableObject, is } from 'vovas-utils';
 
-const prelimSpecs = {
-  title: 'article title',
-  intro: 'article intro',
-  outline: 'section titles (array of strings)',
-} as const;
-
 /**
  * Example function that generates article title, intro, and outline (array of section titles) for given topic.
  * @param topic Topic to generate the article for.
  * @returns An object with `title`, `intro`, and `outline` properties.
  */
 export const generatePrelims = ( topic: string ) =>
-  generate(prelimSpecs, { topic });
+  generate({
+    title: 'article title',
+    intro: 'article intro',
+    outline: 'section titles (array of strings)',
+  } as const, { topic }); 
